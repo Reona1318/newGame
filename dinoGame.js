6cdb3b117c905f74da66cdd57efa1eda2e003734
Reona1318 <71580118+Reona1318@users.noreply.github.com>
2023-06-29 23:12:01 +0900

    Update dinoGame.js

--- dinoGame.js
+++ dinoGame.js
@@ -1,5 +1,8 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-ctx.fillStyle = 'red';
-ctx.fillRect(30, 20, 150, 100);
+const dinoImage = new Image(); // img 要素を作成
+dinoImage.src = `image/dino.png`; // 画像のパスを指定
+dinoImage.onload = () => {
+    ctx.drawImage(dinoImage, 0, 320); // 画像の描画
+}
